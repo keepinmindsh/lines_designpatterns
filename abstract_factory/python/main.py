a2dffd088d50b7f5bf7e70dfedf2ae4555d58b53
Jeong Seung Hwa <keepinmindsh@gmail.com>
2023-09-08 12:27:07 +0900

    added code sample for python abstrac factory first

---
+++ abstract_factory/python/main.py
@@ -0,0 +1,11 @@
+from patterns.abstract_factory import client_code, ConcreteFactory1, ConcreteFactory2
+
+if __name__ == "__main__":
+    print("Client: Testing client code with the first factory type:")
+    client_code(ConcreteFactory1())
+
+    print("\n")
+
+    print("Client: Testing the same client code with the second factory type:")
+    client_code(ConcreteFactory2())
+
