ccd1a6580b6a26d3846d7413e4ef37750ddcd95e
SEUNGHWA JEONG <keepinmindsh@gmail.com>
2023-09-16 15:17:17 +0900

    rust and kotlin code sample for design pattern

--- factory_method/rust/src/main.rs
+++ factory_method/rust/src/main.rs
@@ -1,20 +1,3 @@
-use ferris_says::say; // from the previous step
-use std::io::{stdout, BufWriter};
-#[path = "basic_samples/samples.rs"] mod samples;
-#[path = "basic_samples/struct.rs"] mod structs;
-
-
 fn main() {
-    // let stdout = stdout();
-    // let message = String::from("Hello fellow Rustaceans!");
-    // let width = message.chars().count();
-
-    // let mut writer = BufWriter::new(stdout.lock());
-    // say(&message, width, &mut writer).unwrap();
-
-    samples::printsample();
-
-    samples::arrays_slices();
-
-    structs::structure_sample();
+    
 }
