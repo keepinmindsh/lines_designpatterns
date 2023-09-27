7358f7eb89f6f8a732eaa6efd8bdcc7cf2ced5eb
SEUNGHWA JEONG <keepinmindsh@gmail.com>
2023-09-27 21:38:58 +0900

    prototype - coding samples

--- prototype/go/app/prototype/file.go
+++ prototype/go/app/prototype/file.go
@@ -2,7 +2,6 @@ package prototype
 
 import (
 	"fmt"
-	"prototype/domain"
 )
 
 type FileProto struct {
@@ -13,7 +12,15 @@ func (f *FileProto) print(indentation string) {
 	fmt.Println(indentation + f.Name)
 }
 
+/*
 // todo 해당 코드는 정상적으로 동작하지 않음
 func (f *FileProto) clone() domain.Inode {
 	return &FileProto{Name: f.Name}
+}
+*/
+
+func Clone() *FileProto {
+	fileProto := &FileProto{Name: "Test"}
+
+	return fileProto
 }
