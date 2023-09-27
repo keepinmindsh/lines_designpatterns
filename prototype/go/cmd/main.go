02b9c8c6ebebd1417ea57e8a63d750cf7f653634
SEUNGHWA JEONG <keepinmindsh@gmail.com>
2023-09-27 22:13:55 +0900

    prototype - add pattern samples

--- prototype/go/cmd/main.go
+++ prototype/go/cmd/main.go
@@ -1,5 +1,31 @@
 package cmd
 
+import (
+	"fmt"
+	"prototype/app/prototype"
+	"prototype/domain"
+)
+
 func main() {
-	
+	file1 := &prototype.FileProto{Name: "File1"}
+	file2 := &prototype.FileProto{Name: "File2"}
+	file3 := &prototype.FileProto{Name: "File3"}
+
+	folder1 := &prototype.Folder{
+		Children: []domain.Inode{file1},
+		Name:     "Folder1",
+	}
+
+	folder2 := &prototype.Folder{
+		Children: []domain.Inode{folder1, file2, file3},
+		Name:     "Folder2",
+	}
+
+	fmt.Println("\nPrinting hierarchy for Folder 2")
+	folder2.Print("   ")
+
+	cloneFolder := folder2.Clone()
+	fmt.Println("\nPrinting hierarchy for clone Folder")
+
+	cloneFolder.Print("   ")
 }
